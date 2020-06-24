bc8b4c262461abc88f1a2b7e3f05c42058bae4b7
Dimitrios J. Vergados <vergados@ceid.upatras.gr>
2020-06-24 14:38:37 +0300

    Added initial swagger file

--- eveoauth2.py
+++ eveoauth2.py
@@ -17,6 +17,7 @@
 from eve import Eve
 from oauth2 import BearerAuth
 from flask_sentinel import ResourceOwnerPasswordCredentials, oauth
+from eve_swagger import swagger, add_documentation
 
 app = Eve(auth=BearerAuth)
 ResourceOwnerPasswordCredentials(app)
@@ -26,6 +27,35 @@ ResourceOwnerPasswordCredentials(app)
 def restricted_access():
     return "You made it through and accessed the protected resource!"
 
+app.register_blueprint(swagger)
+# required. See http://swagger.io/specification/#infoObject for details.
+app.config['SWAGGER_INFO'] = {
+    'title': 'My Supercool API',
+    'version': '1.0',
+    'description': 'an API description',
+    'termsOfService': 'my terms of service',
+    'contact': {
+        'name': 'nicola',
+        'url': 'http://nicolaiarocci.com'
+    },
+    'license': {
+        'name': 'BSD',
+        'url': 'https://github.com/pyeve/eve-swagger/blob/master/LICENSE',
+    },
+    'schemes': ['http', 'https'],
+}
+
+
+# optional. Add/Update elements in the documentation at run-time without deleting subtrees.
+add_documentation({'paths': {'/status': {'get': {'parameters': [
+    {
+        'in': 'query',
+        'name': 'foobar',
+        'required': False,
+        'description': 'special query parameter',
+        'type': 'string'
+    }]
+}}}})
 
 if __name__ == '__main__':
     app.run()
